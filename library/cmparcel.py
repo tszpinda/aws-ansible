f036b0e0f117124fae6f01bdb94def756843562a
Tomek Szpinda <tszpinda@gmail.com>
2016-10-04 22:49:46 +0100

    more config changes

--- library/cmparcel.py
+++ library/cmparcel.py
@@ -8,6 +8,7 @@ import json
 import os
 import shlex
 import time
+#import q
 
 from cm_api.api_client import ApiResource
 from cm_api.api_client import ApiException
@@ -16,16 +17,11 @@ from cm_api.api_client import ApiException
 args_file = sys.argv[1]
 args_data = file(args_file).read()
 
-print 'args_file: %s' %args_file
-print 'args_data: %s' %args_data
-
 arguments = shlex.split(args_data)
-print "%s" % arguments
 cm_host = ""
 cluster_name = ""
 cm_cluster_version = ""
 cm_parcel_version = ""
-
 
 for arg in arguments:
     if "=" in arg:
@@ -51,11 +47,11 @@ def waitUntilParcelFound():
   countDown = 1
   while True:
     try:
-      print "parcel found..."
       cluster.get_parcel('CDH', cm_parcel_version)
-      print "parcel found"
-      break
+      return
     except ApiException as err:
+      #q.q('Error ')
+      sys.exit(1)
       if err.code == 404 and countDown > 0:
         countDown = countDown -1
         time.sleep(3)
@@ -71,19 +67,27 @@ def waitUntilParcelFound():
 api = ApiResource(cm_host, username="admin", password="admin")
 cluster = api.get_cluster(cluster_name)
 waitUntilParcelFound()
+#q.q('wait finished')
 
 while True:
+  #q.q('starting while')
   parcel = cluster.get_parcel('CDH', cm_parcel_version)
-  print "parcel"
-  print parcel.state
-  if parcel.stage == 'DISTRIBUTED':
+
+  state = parcel.stage;
+
+  #q.q('>>>>>> Parcel status: ', state)
+
+  if state == 'DISTRIBUTED':
+    #q.q('DISTRIBUTED')
     parcel.activate()
-    break;
-  elif parcel.stage == 'DOWNLOADED':
+  elif state == 'DOWNLOADED':
+    #q.q('DOWNLOADED')
     parcel.start_distribution()
-  elif parcel.stage == 'ACTIVATED':
+  elif state == 'ACTIVATED':
+    #q.q('ACTIVATED')
     break;
-  elif parcel.state == 'AVAILABLE_REMOTELY':
+  elif state == 'AVAILABLE_REMOTELY':
+    #q.q('AVAILABLE_REMOTELY')
     parcel.start_download()
 
   if parcel.state.errors:
